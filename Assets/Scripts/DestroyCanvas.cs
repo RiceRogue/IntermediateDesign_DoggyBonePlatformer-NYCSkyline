6cbc12c8454824d4d226fee6921584c1dea21f2c
Mitu <mitu@glowup.games>
2020-10-16 17:16:32 -0400

    Re-organized project & added a GameManager with scene switcher

---
+++ Assets/Scripts/DestroyCanvas.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DestroyCanvas : MonoBehaviour
+{
+    public Button startButton;
+    public GameObject canvas;
+    public GameObject canvas2;
+    public GameObject canvas3;
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        if (GameManager.Instance.gameStarted)
+        {
+            Debug.Log("Game has started");
+
+            TaskOnClick();
+            canvas3.SetActive(false);
+            canvas2.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Game has not started");
+            startButton.onClick.AddListener(TaskOnClick);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+     void TaskOnClick()
+    {
+        GameObject.Find("CameraFollow").GetComponent<CameraFollowing>().start = true;
+
+        canvas.SetActive(false);
+        canvas2.SetActive(true);
+        
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    public bool gameStarted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+
+        DontDestroyOnLoad(this);
+
+    }
+
+
+        public void ButtonPressedToStartGame()
+        {
+            SceneManager.LoadScene("GAME1");
+            gameStarted = true;
+        }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
